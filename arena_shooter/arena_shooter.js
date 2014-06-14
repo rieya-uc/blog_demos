b93a23cde9d4e2de5b83d8601554605e9d620739
Rieya <rieya.online@gmail.com>
2014-06-14 23:41:39 +0100

    sneaky update, ninja moves to mouse now

--- arena_shooter/arena_shooter.js
+++ arena_shooter/arena_shooter.js
@@ -17,12 +17,29 @@ function preload() {
     game.load.image("ninja", path+"assets/chars/ninja.png");
 }
 
+var ninja;
 function create() {
-    var ninja = game.add.sprite(100,100,"ninja");
+    ninja = game.add.sprite(100,100,"ninja");
     ninja.scale.setTo(0.5, 0.5);
     ninja.anchor.setTo(0.5, 0.5);
+    
+    // enable physics on our player
+    game.physics.enable(ninja, Phaser.Physics.ARCADE);
+    
+    
     game.stage.backgroundColor = '#DDDDDD';
+
+    
 }
 
 function update() {
+    game.physics.arcade.moveToPointer(ninja, 400);
+    //console.log(game.input.position);
+    //  if it's overlapping the mouse, don't move any more
+    if (Phaser.Rectangle.contains(ninja.body, game.input.x, game.input.y)) {
+        //ninja.body.position = game.input.position;
+        ninja.body.velocity.setTo(0, 0);
+        //console.log("risnt");
+    }
+    
 }

b06cf5ec8e962639d077ed99d067e9308bb38dae
Rieya <rieya.online@gmail.com>
2014-06-15 22:56:21 +0100

    better following, hidden cursor

--- arena_shooter/arena_shooter.js
+++ arena_shooter/arena_shooter.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, "demo", {preload:preload, create:create, update:update});
+var game = new Phaser.Game(800, 600, Phaser.AUTO, "demo", {preload:preload, create:create, update:update, render:render});
 
 function preload() {
 
@@ -15,31 +15,54 @@ function preload() {
     game.scale.refresh();
 
     game.load.image("ninja", path+"assets/chars/ninja.png");
+    game.load.image("circle", path+"assets/dashed_circle.png");
+
+
+    // hide the cursor when it's over the game
+    game.canvas.onmouseover = function() {
+        game.canvas.style.cursor = "none";
+    }
+    game.canvas.onmouseout = function() {
+        game.canvas.style.cursor = "default";
+    }
 }
 
-var ninja;
+var ninja, dcircle;
+
 function create() {
     ninja = game.add.sprite(100,100,"ninja");
     ninja.scale.setTo(0.5, 0.5);
     ninja.anchor.setTo(0.5, 0.5);
-    
+
     // enable physics on our player
     game.physics.enable(ninja, Phaser.Physics.ARCADE);
-    
-    
+    ninja.body.allowRotation = false;
+    dcircle = game.add.sprite(150, 150, "circle");
+    dcircle.scale.setTo(0.5, 0.5);
+    dcircle.anchor.setTo(0.5, 0.5);
     game.stage.backgroundColor = '#DDDDDD';
 
     
 }
 
 function update() {
-    game.physics.arcade.moveToPointer(ninja, 400);
-    //console.log(game.input.position);
+
+    dcircle.position = game.input.position;
+    ninja.rotation = game.physics.arcade.moveToPointer(ninja, 400, game.activePointer,50);
+
+
+     /*
+
+
     //  if it's overlapping the mouse, don't move any more
     if (Phaser.Rectangle.contains(ninja.body, game.input.x, game.input.y)) {
-        //ninja.body.position = game.input.position;
         ninja.body.velocity.setTo(0, 0);
-        //console.log("risnt");
+
     }
-    
+    */
+}
+
+function render() {
+    game.debug.body(ninja);
+    game.debug.body(dcircle);
 }

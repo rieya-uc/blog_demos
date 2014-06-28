af9e295d31304ba9c2f5ffaa4cf2152b827e3c85
Rieya <rieya.online@gmail.com>
2014-06-28 22:29:12 +0100

    file added for later

--- follow_the_npc/follow_the_npc.js
+++ follow_the_npc/follow_the_npc.js
@@ -19,18 +19,7 @@ function preload() {
 }
 
 function create() {
-    var k = game.add.sprite(50,100,"npc");
-    k.frame = 0;
-    var l = game.add.sprite(100,100,"npc");
-    l.frame = 1;
-    l.scale.setTo(0.7, 0.7);
-    var m = game.add.sprite(150,100,"npc");
-    m.scale.setTo(0.5, 0.5);
-    m.frame = 3;
-    var n = game.add.sprite(200, 100, "npc");
-    n.scale.setTo(0.3, 0.3);
-    n.frame = 8;
-    
+
 }
 
 function update() {
